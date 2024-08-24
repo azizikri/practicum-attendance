c7fff53f97ad8a735b7ea895f34de64c46263cc9
azizikri <azizikri@gmail.com>
2024-08-25 02:22:18 +0700

    fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -49,7 +49,7 @@
                                                                 <td class="px-4 py-2 text-sm text-gray-700">
                                                                     {{ $attendance['status'] }}</td>
                                                                 <td class="px-4 py-2 text-sm text-gray-700">
-                                                                    {{ $attendance['created_at'] }}</td>
+                                                                    {{ $attendance['created_at'])->diffForHumans() }}</td>
                                                                 <td class="px-4 py-2 text-sm text-gray-700">
                                                                     {{ $attendance['assistant_name'] }}</td>
                                                             </tr>
