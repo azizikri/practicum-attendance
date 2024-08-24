f1ca1d9157065f0c816d49c63820383ed6889357
azizikri <azizikri@gmail.com>
2024-08-25 02:46:31 +0700

    fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,46 +17,51 @@
                                 @foreach ($periods as $period => $schedules)
                                     <h6 class="my-3 text-lg font-semibold">Periode {{ strtoupper($period) }}</h6>
 
-                                    <div class="grid items-center justify-center grid-cols-4 col-span-2">
+                                    <div class="grid grid-cols-1 sm:grid-cols-4 gap-4 items-center justify-center">
                                         @foreach ($schedules as $index => $schedule)
-                                            <h6 class="my-3 text-lg font-semibold">
-                                                {{ $schedule->class_subject_name }}
-                                            </h6>
+                                            <div class="col-span-1 sm:col-span-4">
+                                                <h6 class="my-3 text-lg font-semibold">
+                                                    {{ $schedule->class_subject_name }}
+                                                </h6>
 
-                                            <div class="overflow-x-auto mx-auto">
-                                                <table class="min-w-full bg-white border border-gray-200 mx-auto">
-                                                    <thead class="bg-gray-100">
-                                                        <tr>
-                                                            <th
-                                                                class="px-4 py-2 text-sm font-medium text-left text-gray-700">
-                                                                Pertemuan</th>
-                                                            <th
-                                                                class="px-4 py-2 text-sm font-medium text-left text-gray-700">
-                                                                Presensi</th>
-                                                            <th
-                                                                class="px-4 py-2 text-sm font-medium text-left text-gray-700">
-                                                                Tanggal</th>
-                                                            <th
-                                                                class="px-4 py-2 text-sm font-medium text-left text-gray-700">
-                                                                Asisten yang mengabsen</th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                        @foreach ($schedule->checkAttendances(auth()->user()) as $session => $attendance)
-                                                            <tr class="border-t border-gray-200">
-                                                                <td class="px-4 py-2 text-sm text-gray-700">
-                                                                    {{ $session }}</td>
-                                                                <td class="px-4 py-2 text-sm text-gray-700">
-                                                                    {{ $attendance['status'] }}</td>
-                                                                <td class="px-4 py-2 text-sm text-gray-700">
-                                                                    {{ $attendance['created_at'] == 'Tidak ada Data' ? $attendance['created_at'] : \Carbon\Carbon::parse($attendance['created_at'])->translatedFormat('d F Y') }}
-                                                                </td>
-                                                                <td class="px-4 py-2 text-sm text-gray-700">
-                                                                    {{ $attendance['assistant_name'] }}</td>
+                                                <div class="overflow-x-auto mx-auto">
+                                                    <table class="min-w-full bg-white border border-gray-200 sm:table-fixed">
+                                                        <thead class="bg-gray-100">
+                                                            <tr>
+                                                                <th class="px-4 py-2 text-sm font-medium text-left text-gray-700">
+                                                                    Pertemuan
+                                                                </th>
+                                                                <th class="px-4 py-2 text-sm font-medium text-left text-gray-700">
+                                                                    Presensi
+                                                                </th>
+                                                                <th class="px-4 py-2 text-sm font-medium text-left text-gray-700">
+                                                                    Tanggal
+                                                                </th>
+                                                                <th class="px-4 py-2 text-sm font-medium text-left text-gray-700">
+                                                                    Asisten yang mengabsen
+                                                                </th>
                                                             </tr>
-                                                        @endforeach
-                                                    </tbody>
-                                                </table>
+                                                        </thead>
+                                                        <tbody>
+                                                            @foreach ($schedule->checkAttendances(auth()->user()) as $session => $attendance)
+                                                                <tr class="border-t border-gray-200">
+                                                                    <td class="px-4 py-2 text-sm text-gray-700">
+                                                                        {{ $session }}
+                                                                    </td>
+                                                                    <td class="px-4 py-2 text-sm text-gray-700">
+                                                                        {{ $attendance['status'] }}
+                                                                    </td>
+                                                                    <td class="px-4 py-2 text-sm text-gray-700">
+                                                                        {{ $attendance['created_at'] == 'Tidak ada Data' ? $attendance['created_at'] : \Carbon\Carbon::parse($attendance['created_at'])->translatedFormat('d F Y') }}
+                                                                    </td>
+                                                                    <td class="px-4 py-2 text-sm text-gray-700">
+                                                                        {{ $attendance['assistant_name'] }}
+                                                                    </td>
+                                                                </tr>
+                                                            @endforeach
+                                                        </tbody>
+                                                    </table>
+                                                </div>
                                             </div>
                                         @endforeach
                                     </div>
